6794b354d72c0ee43cdb11e8cd72a2df23b4ca6f
xXG4brielgit config --global user.name xXG4brielgit config --global user.name xXG4briel <gabrielgeorge95@gmail.com>
2021-12-20 23:13:52 -0300

    responsivo e header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,23 +6,57 @@ function Header() {
 
     const headerComponents: string[] =[ 'Sobre', 'Experiências', 'Projetos', 'Contato'];
 
+    function toggleMenu() {
+        let menuComponent = document.querySelector(".header-menu");
+        // console.log();
+        if ( !(menuComponent?.classList.value.split(" ").includes("active")) ) {
+            menuComponent?.classList.add("active");
+            document.querySelector(".header-bottom-container")?.classList.add("active");
+        }
+        else {
+            menuComponent?.classList.remove("active");
+            document.querySelector(".header-bottom-container")?.classList.remove("active");
+        }
+        console.log(menuComponent?.classList);
+    }
+
     return (
         <header>
+            <div className="header-top-container">
+                <div className="header-logo">
+                    <span>xXG4briel</span>
+                </div>
 
-            <div className="header-logo">
-                <span>xXG4briel</span>
+                <section className="header-menu">
+                    <button onClick={ toggleMenu }>
+                        <div id="menu"></div>
+                    </button>
+                </section>
+
+                <section className="header-aboutme">
+                    <nav>
+                        <ul>
+                            {headerComponents.map( headerComponent => 
+                            <li key={headerComponent}>{ headerComponent }</li>
+                            )}
+                        </ul>
+                    </nav>
+                    <button>Contrate-me</button>
+                </section>
             </div>
 
-            <section className="header-aboutme">
-                <nav>
-                    <ul>
-                        {headerComponents.map( headerComponent => 
-                        <li key={headerComponent}>{ headerComponent }</li>
-                        )}
-                    </ul>
-                </nav>
-                <button>Contrate-me</button>
-            </section>
+            <div className="header-bottom-container">
+                <section className="header-aboutme-mobile">
+                    <nav>
+                        <ul>
+                            {headerComponents.map( headerComponent => 
+                            <li key={headerComponent}>{ headerComponent }</li>
+                            )}
+                        </ul>
+                    </nav>
+                </section>
+            </div>
+
 
         </header>
     );
